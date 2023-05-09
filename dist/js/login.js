a90da199a69b56f389e748b24787f0972e86dfb5
yiengling93 <57990936+yiengling93@users.noreply.github.com>
2023-05-09 16:22:10 +0800

    update login.js

--- dist/js/login.js
+++ dist/js/login.js
@@ -8,8 +8,6 @@ function loginPost() {
     var username = document.getElementById('emailAdd').value;
     var password = document.getElementById('password').value;
 
-    username = 'yuie@evonit.net';
-    password = 'asdf!@34';
     //console.log(username);
 
     if (username == '') {

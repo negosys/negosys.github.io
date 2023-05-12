8b66444d02bd6e4933e65de571a09da993ad89fe
yiengling93 <57990936+yiengling93@users.noreply.github.com>
2023-05-12 20:38:02 +0800

    create-project-issues

---
+++ dist/js/issues.js
@@ -0,0 +1,208 @@
+
+var projectNo = "";
+
+$(document).ready(function () {
+    loadIssueKind();
+
+    var queryString = window.location.search;
+    var urlParams = new URLSearchParams(queryString);
+    projectNo = urlParams.get('projectSn');
+
+    loadProjectIssueList();
+});
+
+document
+    .getElementById("btnAddIssue")
+    .addEventListener("click", addIssue);
+
+    document
+    .getElementById("btnNext")
+    .addEventListener("click", nextPage);
+
+    
+function nextPage(){
+    location.href = "prior-issues.html?projectSn=" + projectNo;
+}
+
+function loadIssueKind() {
+    $.ajax({
+        url:
+            'https://api.negosys.co.kr/nego/issueKind',
+        type: "GET",
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        success: function (data) {
+            var issueKind = $("#ddlIssueKind");
+            issueKind.empty();
+            issueKind.append($('<option/>', {
+                value: 0,
+                text: "-Select an issue kind"
+            }));
+            $.each(data, function (index, itemData) {
+                issueKind.append($('<option/>', {
+                    value: itemData.issueKind,
+                    text: itemData.issueKind
+                }));
+            });
+        },
+        error: function (error) {
+            console.log(JSON.stringify(error));
+            Swal.fire({
+                icon: 'error',
+                text: 'Failed to load issue kind.'
+            });
+        }
+    });
+}
+
+function loadProjectIssueList() {
+    dataTable = $('#tblData').DataTable({
+        paging: true,
+        destroy: true,
+        pageLength: 10,
+        scrollX: true,
+        scrollY: true,
+        //order: [[10, 'asc']],
+        "ajax": {
+            url: 'https://api.negosys.co.kr/nego/issuesByProjectSn',
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            data: { projectSn: projectNo },
+            dataSrc: "",
+        },
+        "columns": [
+            { "data": "issueKind", "width": "30%" },
+            { "data": "issue", "width": "40%" },
+            {
+                "data": "issueSn",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a onclick=deleteIssue(${data}) class="btn btn-info text-white" style="cursor:pointer">
+                                    <i class="fa fa-trash-alt"></i> 
+                                </a>
+                            </div>
+                           `;
+                }, "width": "30%"
+            }
+        ]
+    });
+
+}
+
+$('#ddlIssueKind').on('select2:select', function (e) {
+    var selectedIssueKind = e.params.data.id;
+    $.ajax({
+        url: 'https://api.negosys.co.kr/nego/issueKind',
+        type: 'GET',
+        success: function (data) {
+            var issue = $("#ddlIssue");
+            issue.empty();
+            issue.append($('<option/>', {
+                value: 0,
+                text: "-Select an issue"
+            }));
+            $.each(data, function (index, itemData) {
+                if (itemData.issueKind == selectedIssueKind) {               
+                    $.each(itemData.issueList, function (i, issueData) {
+                        console.log(issueData);
+                        issue.append($('<option/>', {
+                            value: issueData,
+                            text: issueData
+                        }));
+                    });
+                }
+            });
+        },
+        error: function (e) {
+            console.log(e);
+            Swal.fire({
+                icon: 'error',
+                text: 'Failed to retrieve Issues List!'
+            });
+        }
+    });
+});
+
+function addIssue() {
+    var issueKind = document.getElementById("ddlIssueKind").options[document.getElementById("ddlIssueKind").selectedIndex].value;
+    var issue = document.getElementById("ddlIssue").options[document.getElementById("ddlIssue").selectedIndex].value;
+    
+    if ($("#ddlIssueKind").val() == "") {
+        Swal.fire({
+            icon: 'warning',
+            text: 'No Issue Kind is selected'
+        });
+        return;
+    } else if ($("#ddlIssue").val() == "") {
+        Swal.fire({
+            icon: 'warning',
+            text: 'No Issue is selected'
+        });
+        return;
+    }
+
+    let reqObj = { projectSn: projectNo, issueKind: issueKind, issue: issue }
+
+    $.ajax({
+        url:
+            'https://api.negosys.co.kr/nego/issue',
+        type: "POST",
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        data: JSON.stringify(reqObj),
+        contentType: "application/json",
+        success: function (data) {
+            var x = JSON.stringify(data);
+            console.log(x);
+            loadProjectIssueList();
+        },
+        error: function (error) {
+            console.log(JSON.stringify(error));
+            Swal.fire({
+                icon: 'error',
+                text: 'Failed to add issue'
+            });
+        }
+    });
+}
+
+function deleteIssue(issueSn) {
+    
+    if (issueSn == '') {
+        Swal.fire({
+            icon: 'warning',
+            text: 'Cannot get Issue Number.'
+        });
+        return;
+    }
+
+    $.ajax({
+        url:
+            'https://api.negosys.co.kr/nego/issue',
+        type: "DELETE",
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        data: { issueSn: issueSn },
+        success: function (data) {
+            var x = JSON.stringify(data);
+            console.log(x);
+            loadProjectIssueList();
+        },
+        error: function (error) {
+            console.log(JSON.stringify(error));
+            Swal.fire({
+                icon: 'error',
+                text: 'Failed to delete issue'
+            });
+        }
+    });
+}
---
+++ dist/js/projects.js
@@ -0,0 +1,144 @@
+
+
+
+$(document).ready(function () {
+    loadProjectList();
+
+});
+
+document
+    .getElementById("btnCreateProj")
+    .addEventListener("click", createProject);
+
+function loadProjectList() {
+    dataTable = $('#tblData').DataTable({
+        paging: true,
+        destroy: true,
+        pageLength: 10,
+        scrollX: true,
+        scrollY: true,
+        //order: [[10, 'asc']],
+        "ajax": {
+            url: 'https://api.negosys.co.kr/nego/projectsByUser',
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            dataSrc: "",
+        },
+        "columns": [
+            {
+                "data": 'createDt',
+                "type": 'date',
+                render: function (data, type, row) {
+                    return data ? moment(data).format('DD/MM/YYYY') : '';
+                },
+                "width": "10%"
+            },
+            { "data": "projectNm", "width": "20%" },
+            { "data": "projectDesc", "width": "20%" },
+            {
+                "data": "projectSn",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="issues.html?projectSn=${data}" class="btn btn-default" style="cursor:pointer">
+                                    Update 
+                                </a>
+                                <a href="issues.html?projectSn=${data}" class="btn btn-default" style="cursor:pointer">
+                                    Issue Tracking 
+                                </a>
+                                <a onclick=Delete("/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fa fa-trash-alt"></i> 
+                                </a>
+                            </div>
+                           `;
+                }, "width": "50%"
+            }
+        ]
+    });
+
+}
+
+function createProject() {
+    var pjName = document.getElementById('project-name').value;
+    var pjDesc = document.getElementById('project-desc').value;
+
+    if (pjName == '') {
+        Swal.fire({
+            icon: 'warning',
+            text: 'Please enter Project Name'
+        });
+        return;
+    } else if (pjDesc == '') {
+        Swal.fire({
+            icon: 'warning',
+            text: 'Please enter Project Description'
+        });
+        return;
+    }
+
+    let reqObj = { projectNm: pjName, projectDesc: pjDesc }
+
+    $.ajax({
+        url:
+            'https://api.negosys.co.kr/nego/project',
+        type: "POST",
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        data: JSON.stringify(reqObj),
+        contentType: "application/json",
+        success: function (data) {
+            var x = JSON.stringify(data);
+            console.log(x);
+            loadProjectList();
+            $('#createModal').modal('hide');
+            document.getElementById('project-name').value = "";
+            document.getElementById('project-desc').value = "";
+        },
+        error: function (error) {
+            console.log(JSON.stringify(error));
+            Swal.fire({
+                icon: 'error',
+                text: 'Failed to create project'
+            });
+        }
+    });
+}
+
+function deleteIssue() {
+    var issueSn = document.getElementById('project-name').value;
+
+    if (issueSn == '') {
+        Swal.fire({
+            icon: 'warning',
+            text: 'Cannot get Issue Number.'
+        });
+        return;
+    }
+
+    $.ajax({
+        url:
+            'https://api.negosys.co.kr/nego/issue',
+        type: "DELETE",
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        data: { issueSn: issueSn },
+        success: function (data) {
+            var x = JSON.stringify(data);
+            console.log(x);
+            loadProjectList();
+        },
+        error: function (error) {
+            console.log(JSON.stringify(error));
+            Swal.fire({
+                icon: 'error',
+                text: 'Failed to delete issue'
+            });
+        }
+    });
+}
